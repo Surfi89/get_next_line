9a3a85a9d69548372e5263c609d3a3dd0a347d04
Aritz Jordan zaballa <ajordan-@c3r3s3.42urduliz.com>
2021-08-18 10:42:18 +0200

    primer commit, crear archivos necesarios

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajordan- <ajordan-@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/16 10:12:35 by ajordan-          #+#    #+#             */
+/*   Updated: 2021/08/18 10:03:26 by ajordan-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+
+#endif
